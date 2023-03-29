dd19d32bff3fa670487f82fb7e4959e9c5a7c565
kwyntes <78665317+kwyntes@users.noreply.github.com>
2023-03-29 20:29:07 +0200

    Update windows_code.ps1

--- windows_code.ps1
+++ windows_code.ps1
@@ -1,6 +1,6 @@
 # This could be an install script for example
-# Note that PowerShell is cross-platform nowadays so to be absolutely sure
-# we should still perform an OS check here.
+# Note there exists a cross-platform PowerShell nowadays, so to
+# be absolutely sure we should still perform an OS check here.
 
 echo Running through PowerShell (or Batch / Command Prompt)
 echo We\'re most likely on Windows now
