16def414c1a140e1ea0aa3af7970c5e283116f96
kwyntes <78665317+kwyntes@users.noreply.github.com>
2023-03-29 20:25:32 +0200

    Create windows_code.ps1

---
+++ windows_code.ps1
@@ -0,0 +1,6 @@
+# This could be an install script for example
+# Note that PowerShell is cross-platform nowadays so to be absolutely sure
+# we should still perform an OS check here.
+
+echo Running through PowerShell (or Batch / Command Prompt)
+echo We\'re most likely on Windows now
